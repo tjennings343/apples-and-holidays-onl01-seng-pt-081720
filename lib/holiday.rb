1d62932a67c43485fc664682a35e6988c9619e27
Trey Jennings <tjennings343@gmail.com>
2020-08-29 04:27:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,8 +40,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[season][holiday_name] = supply_array
-  holiday_hash
+  holiday_hash[:fall][:columbus_day]
   # code here
   # remember to return the updated hash
 
