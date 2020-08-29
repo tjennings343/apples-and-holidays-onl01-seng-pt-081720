4a4c6ab3f82e4a7a1958d066f21cc20ea108c5ff
Trey Jennings <tjennings343@gmail.com>
2020-08-29 04:19:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:memorial_day] << supply
+  holiday_hash[:memorial_day] << "supply"
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
