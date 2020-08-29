3f586e87c883c821b9d4dadad3d1293612cff047
Trey Jennings <tjennings343@gmail.com>
2020-08-29 04:12:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:fourth_of_july][2]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -61,10 +62,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
