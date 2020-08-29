82ec9a2815c1939f56d6ca5f7cd2f9aac47871b8
Trey Jennings <tjennings343@gmail.com>
2020-08-29 04:16:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash[:fourth_of_july][2]
+  holiday_supplies[:summer][:fourth_of_july][1]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
